d151a9e7301a42ef8c904dad041ea2342e3fc650
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2021-09-16 20:12:53 -0500

    IoC plain Java

--- src/main/java/com/spring/runner/Main.java
+++ src/main/java/com/spring/runner/Main.java
@@ -1,9 +1,15 @@
 package com.spring.runner;
 
+import com.spring.business.spi.CustomerBusiness;
+import com.spring.business.spi.CustomerDto;
+import com.spring.ioc.IoCFactory;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    CustomerBusiness customerBusiness = IoCFactory.getCustomerBusiness();
+    CustomerDto customer = customerBusiness.findById("X");
+    System.out.println("Su nombre es: " + customer.getName());
   }
 
 }
